71644d924dfe603b5317a5fc1dee69318db1fd33
simyanyi <yanyi.sim.2021@scis.smu.edu.sg>
2022-08-29 22:33:57 +0800

    hi

--- operation.js
+++ operation.js
@@ -1,11 +1,11 @@
 //Get the button:
-mybutton = document.getElementById("top");
+mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
