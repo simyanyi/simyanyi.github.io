8d93c0a8dd0f391c3a24ef7a6b8ef6c133790f28
Yan Yi <simyanyi2002@gmail.com>
2022-09-16 23:57:55 +0800

    Edit new image above timeline

--- operation.js
+++ operation.js
@@ -2,7 +2,7 @@
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -18,3 +18,13 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+// change image under edu
+function changeImage1() {
+  document.getElementById('photo').src = "image/ahs.JPG"
+}
+function changeImage2() {
+  document.getElementById('photo').src = "image/vjc.JPG"
+}
+function changeImage3() {
+  document.getElementById('photo').src = "image/smu.JPG"
+}
